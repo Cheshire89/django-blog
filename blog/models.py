5770f7a188cdad4e648724529ce950d2a1b708bf
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-11-12 14:19:43 -0700

    get posts by slug

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 
 class PostManager(models.Manager):
@@ -16,7 +17,10 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique=True
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -43,3 +47,8 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.slug
+        ])
