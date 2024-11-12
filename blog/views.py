64b1ea8f4e7ad538774d0f659159dd7250e0fe5a
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-11-12 14:49:12 -0700

    wireup pagination

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,40 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
-from django.http import Http404
 from django.shortcuts import get_object_or_404
+# from django.views.generic import ListView
 from .models import Post
+from pprint import pprint
 # Create your views here.
 
 
+# class PostListView(ListView):
+#     '''Alternative posts list view'''
+
+#     queryset = Post.objects.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+
+
 def post_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+
     return render(
         request,
         'blog/post/list.html',
         {'posts': posts}
     )
 
-# def post_list_by_author(request):
-#     posts = Post.objects.all()
 
 def post_detail(request, post):
     post = get_object_or_404(
