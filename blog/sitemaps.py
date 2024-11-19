edab42c680889919a842e45d365814f25bf93ed3
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-11-18 21:45:16 -0700

    add tag urls to sitemap

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,5 +1,7 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Post
+from django.urls import reverse
+from taggit.models import Tag
 
 
 class PostSitemap(Sitemap):
@@ -12,3 +14,16 @@ class PostSitemap(Sitemap):
 
     def lastmod(self, obj: Post):
         return obj.updated
+
+
+class TagSitemap(Sitemap):
+    changefreq = 'monthly'
+    priority = 0.9
+
+    def items(self):
+        # Return all tags used in the blog
+        return Tag.objects.all()
+
+    def location(self, obj):
+        # Generate URL for the tag-filtered view
+        return reverse('blog:post_list_by_tag', kwargs={'tag_slug': obj.slug})
